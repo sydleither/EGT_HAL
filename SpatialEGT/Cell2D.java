90a54557f7027c26cbc6ec4236343c81b5a91ac0
sydleither <sydleither@protonmail.com>
2026-03-31 15:49:07 -0400

    fix average payoff around empty cells

--- SpatialEGT/Cell2D.java
+++ SpatialEGT/Cell2D.java
@@ -19,9 +19,12 @@ public class Cell2D extends AgentSQ2Dunstackable<Model2D> {
             Cell2D neighborCell = G.GetAgent(G.interactHood[i]);
             total_payoff += G.payoffMatrix[this.type][neighborCell.type];
         }
-        int empty = MapEmptyHood(G.interactHood);
-        total_payoff += empty * G.payoffMatrix[this.type][G.numTypes];
-        return total_payoff / (neighbors + empty);
+        if (G.payoffMatrix[this.type][G.numTypes] != 0.0) {
+            int empty = MapEmptyHood(G.interactHood);
+            total_payoff += empty * G.payoffMatrix[this.type][G.numTypes];
+            neighbors += empty;
+        }
+        return total_payoff / neighbors;
     }
 
     public void CellStep() {
